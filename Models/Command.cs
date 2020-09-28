23757b54e86e7f359960ca09594cc09f28548fe0
Preston Carroll <plutusinc1@yahoo.com>
2020-09-27 19:12:37 -0500

    test

---
+++ Models/Command.cs
@@ -0,0 +1,9 @@
+namespace MyApi.Models
+{
+    public class Command {
+        public int id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
